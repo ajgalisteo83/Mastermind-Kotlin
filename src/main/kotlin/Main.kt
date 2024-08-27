824e3c5b631a95fa8a1dc6d030178f1262bcff0c
Antonio J. Galisteo <ajgalisteo83@gmail.com>
2024-08-27 18:28:07 +0200

    Get colors to guess

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,5 +10,27 @@ fun main() {
         println("indice: $index, color: $color")
         colors[index] = color.toString()
     }
-    println(colors)
+    println("Colors $colors")
+    print("Max number of attempts: ")
+    val maxNumberAttempts = readln().toInt()
+    println("Try to guess the colors!")
+    var success = false
+    var attempts = 0
+    while (attempts < maxNumberAttempts && !success) {
+        print("Enter 5 colors separated by comas: ")
+        val guessColors = readln().split(",")
+        var guess: MutableList<String> = Array(5) {""}.toList().toMutableList()
+        guessColors.forEachIndexed { index, color ->
+            guess[index] = color.toString()
+        }
+        println("Guess $guess")
+        if (colors == guess) {
+            success = true
+            println("Congratulations! You guessed the colors!")
+        } else {
+            attempts++
+            println("You have $attempts attempts")
+        }
+    }
+
 }
